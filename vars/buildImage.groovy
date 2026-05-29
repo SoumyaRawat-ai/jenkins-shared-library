dc16be8dd5cc58164d481b8e7cb7f927d1f42703
Soumya Rawat <soumyarawat0305@gmail.com>
2026-05-29 19:56:26 +0530

    Refactor Docker image build script
    Refactor the buildImage.groovy script for better readability.

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,11 +1,24 @@
 #!/usr/bin/env groovy
 
 def call() {
-   echo "Building the Docker image..."
-    withCredentials([usernamePassword(credentialsId: 'docker-hub',passwordVariable: 'PASS', usernameVariable: 'USER' )]) {
-    sh 'docker build -t soumyarawat03/demo-app:jma2.1 .'
-    sh "echo $PASS | docker login -u $USER --password-stdin"
-    sh 'docker push soumyarawat03/demo-app:jma2.1'
-  }
+
+    echo "Building the Docker image..."
+
+    withCredentials([
+        usernamePassword(
+            credentialsId: 'docker-hub',
+            usernameVariable: 'USER',
+            passwordVariable: 'PASS'
+        )
+    ]) {
+
+        sh 'docker build -t soumyarawat03/demo-app:jma2.1 .'
+
+        sh """
+            echo \$PASS | docker login -u \$USER --password-stdin
+        """
+
+        sh 'docker push soumyarawat03/demo-app:jma2.1'
+    }
 }
 
